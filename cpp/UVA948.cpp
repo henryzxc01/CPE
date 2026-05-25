732e433fe37f8aac8027e5bba0aaf43cfbdecc3e
Henry <henry940509@gmail.com>
2026-05-25 12:14:48 +0800

    更新 UVA948.cpp

--- cpp/UVA948.cpp
+++ cpp/UVA948.cpp
@@ -1,40 +1,41 @@
 //https://zerojudge.tw/ShowProblem?problemid=a134
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-    // 競賽常用：加速 I/O
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    // 預先建表：題目測資最大到 100,000,000，算到第 39 項即足夠
-    int fib[40];
-    fib[0] = 1; // 費氏數列在 Zeckendorf 定理中從 1, 2 開始算
-    fib[1] = 2;
-    for (int i = 2; i < 40; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
+    vector<int> fib = {1, 2};
+
+    while (fib.back() <= 100000000) {
+        int n = fib.size();
+        fib.push_back(fib[n - 1] + fib[n - 2]);
     }
 
-    int n, num;
-    if (cin >> n) {
-        while (n--) {
-            cin >> num;
-            cout << num << " = ";
+    int T;
+    cin >> T;
 
-            int temp = num;
-            bool started = false; // 用來標記是否已經遇到第一個 '1'，避免印出前導零
+    while (T--) {
+        int x;
+        cin >> x;
 
-            // 從最大的費氏數往下找
-            for (int i = 39; i >= 0; i--) {
-                if (temp >= fib[i]) {
-                    cout << "1";
-                    temp -= fib[i];
-                    started = true;
-                } else if (started) {
-                    cout << "0";
-                }
+        cout << x << " = ";
+
+        bool started = false;
+
+        for (int i = (int)fib.size() - 1; i >= 0; i--) {
+            if (fib[i] <= x) {
+                cout << '1';
+                x -= fib[i];
+                started = true;
+            } else if (started) {
+                cout << '0';
             }
-            cout << " (fib)\n";
         }
+
+        cout << " (fib)\n";
     }
+
     return 0;
 }
