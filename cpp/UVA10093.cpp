a6a871a3cec72ffa2e88614341327ff073735428
Henry <henry940509@gmail.com>
2026-05-26 02:31:50 +0800

    Update UVA10093.cpp

--- cpp/UVA10093.cpp
+++ cpp/UVA10093.cpp
@@ -1,43 +1,39 @@
 //https://vjudge.net/problem/UVA-10093
-#include <iostream>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    // 優化輸入輸出速度（競賽常用）
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     string s;
     while (cin >> s) {
-        int sum = 0, mx = 1; // mx 預設為 1，確保最小從 2 進位開始計算
-        
-        // 計算各位數字的十進位總和，並找出出現過的最大字元值
+        int sum = 0, mx = 1;
+
         for (char c : s) {
-            int v = 0;
-            if (c >= '0' && c <= '9') v = c - '0';
-            else if (c >= 'A' && c <= 'Z') v = c - 'A' + 10;
-            else if (c >= 'a' && c <= 'z') v = c - 'a' + 36;
-            else continue; // 忽略正負號或其他非英數符號
-            
-            sum += v;
-            if (v > mx) mx = v;
+            int v = -1;
+
+            if ('0' <= c && c <= '9') v = c - '0';
+            else if ('A' <= c && c <= 'Z') v = c - 'A' + 10;
+            else if ('a' <= c && c <= 'z') v = c - 'a' + 36;
+
+            if (v != -1) {
+                sum += v;
+                mx = max(mx, v);
+            }
         }
-        
-        // 尋找符合條件的最小進位制 (N)
+
         int ans = 0;
-        for (int i = mx + 1; i <= 62; ++i) {
-            if (sum % (i - 1) == 0) {
-                ans = i;
+        for (int base = mx + 1; base <= 62; base++) {
+            if (sum % (base - 1) == 0) {
+                ans = base;
                 break;
             }
         }
-        
-        // 輸出結果
-        if (ans) cout << ans << "\n";
+
+        if (ans) cout << ans << '\n';
         else cout << "such number is impossible!\n";
     }
-    
+
     return 0;
 }
