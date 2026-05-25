1ddf7bc91fda743f7899344cf7ef095eb113d802
Henry <henry940509@gmail.com>
2026-05-25 22:18:24 +0800

    Update UVA10050.cpp

--- cpp/UVA10050.cpp
+++ cpp/UVA10050.cpp
@@ -2,31 +2,47 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
- 
+
 int main() {
-    int T, N, P, h;
-    cin >> T;
-    while (T--){
-        cin >> N >> P;
-        int a[N+1];
-        memset(a, 0, sizeof(a));
-        for (int i = 0; i < P; i++){
-            cin >> h;
-            for (int j = h; j <= N; j+=h){
-                a[j] = 1;
+    int testCases;
+    cin >> testCases;
+
+    while (testCases--) {
+        int days, parties;
+        cin >> days >> parties;
+
+        int isHartalDay[3700];
+        memset(isHartalDay, 0, sizeof(isHartalDay));
+
+        // 標記每個政黨造成的罷工日
+        for (int i = 0; i < parties; i++) {
+            int hartalParameter;
+            cin >> hartalParameter;
+
+            for (int day = hartalParameter; day <= days; day += hartalParameter) {
+                isHartalDay[day] = 1;
             }
         }
-        for (int i = 6; i <= N; i+=7){
-            a[i] = 0;
+
+        // 題目規定：星期五和星期六不算損失工作日
+        // 第 1 天是星期日，所以第 6, 13, 20... 天是星期五
+        for (int day = 6; day <= days; day += 7) {
+            isHartalDay[day] = 0;
         }
-        for (int i = 7; i <= N; i+=7){
-            a[i] = 0;
+
+        // 第 7, 14, 21... 天是星期六
+        for (int day = 7; day <= days; day += 7) {
+            isHartalDay[day] = 0;
         }
-        int sum = 0;
-        for (int i = 1; i <= N; i++){
-            sum += a[i];
+
+        int lostWorkingDays = 0;
+
+        for (int day = 1; day <= days; day++) {
+            lostWorkingDays += isHartalDay[day];
         }
-        cout << sum << "\n";
+
+        cout << lostWorkingDays << '\n';
     }
+
     return 0;
 }
