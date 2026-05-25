e89ac5ea6b747c696713a962e8baaae084048047
Henry <henry940509@gmail.com>
2026-05-25 10:40:07 +0800

    更新 UVA272.cpp

--- cpp/UVA272.cpp
+++ cpp/UVA272.cpp
@@ -1,28 +1,22 @@
-#include <iostream>
-#include <string>
-
+//https://zerojudge.tw/ShowProblem?problemid=c007
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    // 競賽常用的 I/O 加速
-    ios::sync_with_stdio(false); 
-    cin.tie(NULL);
-    
-    string s;
-    bool open = true; // 狀態可以跨行保留
-    
-    // 逐行讀取直到 EOF
-    while (getline(cin, s)) {
-        for (char c : s) {
-            if (c == '"') {
-                cout << (open ? "``" : "''");
-                open = !open;
-            } else {
-                cout << c;
-            }
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    char c;
+    bool open = true;
+
+    while (cin.get(c)) {
+        if (c == '"') {
+            cout << (open ? "``" : "''");
+            open = !open;
+        } else {
+            cout << c;
         }
-        cout << '\n'; // getline 會吃掉換行符號，需要手動補上
     }
-    
+
     return 0;
 }
