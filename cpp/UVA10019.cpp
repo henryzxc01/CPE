70e722c26fd01b65af9c60a8640a5cfeb2828839
Henry <henry940509@gmail.com>
2026-05-25 13:37:50 +0800

    更新 UVA10019.cpp

--- cpp/UVA10019.cpp
+++ cpp/UVA10019.cpp
@@ -1,33 +1,27 @@
 //https://zerojudge.tw/ShowProblem?problemid=e545
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    // 優化輸入輸出速度（競賽常用）
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int t, n;
-    if (cin >> t) {
-        while (t--) {
-            cin >> n;
+    int T, n;
+    cin >> T;
 
-            // b1: 將 n 視為十進位時，二進位表示法中 1 的數量
-            int b1 = __builtin_popcount(n);
+    while (T--) {
+        cin >> n;
 
-            // b2: 將 n 視為十六進位時，二進位表示法中 1 的數量
-            int b2 = 0;
-            int temp = n;
-            while (temp > 0) {
-                // 取出十進位的每一個數字當作十六進位的位數，並計算該數字轉二進位後 1 的數量
-                b2 += __builtin_popcount(temp % 10);
-                temp /= 10;
-            }
+        int b1 = __builtin_popcount(n);
 
-            // 輸出結果
-            cout << b1 << " " << b2 << "\n";
+        int b2 = 0;
+        while (n) {
+            b2 += __builtin_popcount(n % 10);
+            n /= 10;
         }
+
+        cout << b1 << ' ' << b2 << '\n';
     }
+
     return 0;
 }
