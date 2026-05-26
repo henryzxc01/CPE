688ce56b440e9d06aab9a8e622d4f47ed5822c2b
Henry <henry940509@gmail.com>
2026-05-26 17:10:03 +0800

    更新 UVA10222.cpp

--- cpp/UVA10222.cpp
+++ cpp/UVA10222.cpp
@@ -1,27 +1,20 @@
 //https://zerojudge.tw/ShowProblem?problemid=e578
-#include <iostream>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    string k = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
+    string kb = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
     string s;
 
     while (getline(cin, s)) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == ' ') {
-                cout << " ";
-            } else {
-                for (int j = 0; j < k.length(); j++) {
-                    if (k[j] == s[i]) {
-                        cout << k[j - 2];
-                        break;
-                    }
-                }
+        for (char c : s) {
+            if (c == ' ') cout << c;
+            else {
+                int p = kb.find(c);
+                cout << kb[p - 2];
             }
         }
-        cout << endl;
+        cout << '\n';
     }
     return 0;
 }
