845f3127473622c4c1613c6a0e6ad4c50bc12380
Henry <henry940509@gmail.com>
2026-05-26 11:09:28 +0800

    更新 UVA10170.cpp

--- cpp/UVA10170.cpp
+++ cpp/UVA10170.cpp
@@ -1,23 +1,20 @@
 //https://zerojudge.tw/ShowProblem?problemid=e555
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    // 競賽常用的 I/O 加速
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
     long long S, D;
-    // 不斷讀取直到 EOF
+
     while (cin >> S >> D) {
-        // 當剩餘天數 D 還大於當前團體人數 S 時
-        while (D > S) {
-            D -= S++; // 扣除當前團體停留的天數，並將人數直接 +1
+        long long sum = 0;
+
+        while (sum < D) {
+            sum += S;
+            S++;
         }
-        cout << S << "\n";
+
+        cout << S - 1 << '\n';
     }
+
     return 0;
 }
-
-
