ecbf2ef2b98f2806cef54b6f3db48f07f54af9fc
Henry <henry940509@gmail.com>
2026-05-26 10:44:15 +0800

    更新 UVA10101.cpp

--- cpp/UVA10101.cpp
+++ cpp/UVA10101.cpp
@@ -1,52 +1,39 @@
 //https://zerojudge.tw/ShowProblem?problemid=a741
-#include <iostream>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    // 競賽常用的 I/O 加速
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    long long n;
-    int t = 1;
-    // 將對應的數值與字串建表
-    long long d[] = {10000000, 100000, 1000, 100};
-    string s[] = {" kuti", " lakh", " hajar", " shata"};
-    
-    while (cin >> n) {
-        cout << setw(4) << t++ << ".";
-        
-        if (n == 0) {
-            cout << " 0\n";
-            continue;
-        }
-        
-        long long H = n / 10000000; // 高位部分 (大於等於 kuti)
-        long long L = n % 10000000; // 低位部分 (小於 kuti)
-        
-        // 處理高位部分
-        if (H > 0) {
-            for (int i = 0; i < 4; i++) {
-                if (H >= d[i]) {
-                    cout << " " << H / d[i] << s[i];
-                    H %= d[i];
-                }
-            }
-            if (H > 0) cout << " " << H;
-            cout << " kuti"; // 高位處理完必定要補上 kuti 單位
-        }
-        
-        // 處理低位部分
-        for (int i = 0; i < 4; i++) {
-            if (L >= d[i]) {
-                cout << " " << L / d[i] << s[i];
-                L %= d[i];
-            }
-        }
-        if (L > 0) cout << " " << L;
-        
-        cout << "\n";
+void f(long long n) {
+    if (n >= 10000000) {
+        f(n / 10000000);
+        cout << " kuti";
+        n %= 10000000;
     }
-    return 0;
+    if (n >= 100000) {
+        f(n / 100000);
+        cout << " lakh";
+        n %= 100000;
+    }
+    if (n >= 1000) {
+        f(n / 1000);
+        cout << " hajar";
+        n %= 1000;
+    }
+    if (n >= 100) {
+        f(n / 100);
+        cout << " shata";
+        n %= 100;
+    }
+    if (n) cout << " " << n;
+}
+
+int main() {
+    long long n;
+    int tc = 1;
+
+    while (cin >> n) {
+        cout << setw(4) << tc++ << ".";
+        if (n == 0) cout << " 0";
+        else f(n);
+        cout << '\n';
+    }
 }
